b84517299e9465c79f980601d4ee6aadd091f436
junfuture1103 <junhak1103@naver.com>
2022-08-25 13:21:48 +0900

    Trouble shoot numpy -> torch

--- src/datasets/full_dataset.py
+++ src/datasets/full_dataset.py
@@ -8,7 +8,8 @@ class FullDataset(datasets.BasicDataset):
     def __init__(self, training: bool = True, transform: Callable = None, target_transform: Callable = None):
         super().__init__(training, transform, target_transform)
         self.samples = datasets.training_samples if training else datasets.test_samples
-        self.samples = torch.from_numpy(self.samples).float()
+        # self.samples = torch.from_numpy(self.samples).float()
+        self.samples = self.samples.float()
         
         self.labels = datasets.training_labels if training else datasets.test_labels
-        self.labels = torch.from_numpy(self.labels).float()
+        self.labels = self.labels.float()
